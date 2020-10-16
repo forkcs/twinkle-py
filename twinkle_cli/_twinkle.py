b23c55d0924580ef3a8dcc89ca3ca9fa373de05f
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-16 14:59:33 +0300

    Write docstrings and typehints.

--- twinkle_cli/_twinkle.py
+++ twinkle_cli/_twinkle.py
@@ -1,3 +1,4 @@
+from typing import Iterator, List
 from subprocess import Popen, PIPE
 from multiprocessing import Process, Event
 
@@ -9,7 +10,12 @@ class Twinkle:
     Call run() before using any sip methods.
     """
 
-    def __init__(self, cmd: list = None):
+    def __init__(self, cmd: List[str] = None):
+        """
+
+        :param cmd: command to execute. defaults to ['twinkle', '-c']
+        """
+
         if cmd is None:
             cmd = ['twinkle', '-c']
         self.cmd = cmd
@@ -17,38 +23,44 @@ class Twinkle:
         self.proc: Popen = None
         self.stdout_reader: Process = None
 
-        self._stop_print_event = Event()
+        self._stop_reading_event = Event()
 
-    def print_stdout(self) -> None:
+    def read_stdout(self) -> Iterator:
+        """Generator, yields one line from stdout."""
 
         while True:
-            if self._stop_print_event.is_set():
+            if self._stop_reading_event.is_set():
                 break
             line = self.proc.stdout.readline()
-            print(line.decode())
+            yield line.decode()
 
-    def stop_print(self) -> None:
-        self._stop_print_event.set()
+    def stop_reading(self) -> None:
+        """Ask stdout reader process to exit."""
+
+        self._stop_reading_event.set()
         self.stdout_reader.join()
 
     def run(self) -> None:
+        """Start Twinkle and stdout reader processes."""
+        self._stop_reading_event.clear()
+
         self.proc = Popen(
             self.cmd,
             stdin=PIPE,
             stdout=PIPE,
             stderr=PIPE
         )
-        self.stdout_reader = Process(target=self.print_stdout)
+        self.stdout_reader = Process(target=self.read_stdout)
         self.stdout_reader.start()
 
-    def kill(self) -> None:
-        self.stop_print()
-        self.proc.kill()
-
     def send_command(self, command: str) -> None:
+        """Send any string to stdin then line break."""
+
         self.proc.stdin.write(f'{command}\n'.encode())
         self.proc.stdin.flush()
 
-    def stop_proc(self) -> None:
+    def stop_twinkle(self) -> None:
+        """Send 'quit' to Twinkle CLI."""
+
         self.send_command('quit')
-        self.stop_print()
+        self.stop_reading()
